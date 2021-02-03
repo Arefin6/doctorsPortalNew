f38d2eb1a6c8fe7f9778427d61782111924fd406
Arefi6 <arefinhossain3@gmail.com>
2021-02-03 20:24:08 +0600

    user andappointment model done

---
+++ backend/models/appointmentModel.js
@@ -0,0 +1,45 @@
+import  mongoose  from 'mongoose'
+
+
+const appointmentSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true,
+    },
+    age:{
+        type:String,
+        required:true,
+    },
+    weight:{
+        type:String,
+        required:true,
+    }
+    ,service:{
+        type:String,
+        required:true,
+    },
+    date:{
+        type:Date,
+        required:true,
+    }
+
+ },{
+   timestamps:true 
+ }
+)
+
+const Appointment = mongoose.model('Appointment',appointmentSchema)
+
+export default Appointment;
