92c8f3089bc6b4726668813eb7d0f9190b8b1e2d
Arefi6 <arefinhossain3@gmail.com>
2021-02-04 22:24:04 +0600

    react-router set up done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import {Container} from 'react-bootstrap';
+import HomeScreen from '../src/components/screens/HomeScreen/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Router>
+       <Container>
+          <Switch>
+            <Route path='/' exact>
+              <HomeScreen></HomeScreen>
+            </Route>
+          </Switch>
+         </Container> 
+      </Router> 
+    </>
   );
 }
 
---
+++ client/src/components/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+        <>
+         <h2 className="text-info">Welcome to Doctors Portal</h2>   
+        </>
+    );
+};
+
+export default HomeScreen;
