a6341904f675885292913b840a155842705788b8
Arefi6 <arefinhossain3@gmail.com>
2021-02-09 13:58:52 +0600

    Sign up screen done

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import HomeScreen from '../src/components/screens/HomeScreen/HomeScreen'
 import LoginScreen from "./components/screens/LoginScreen/LoginScreen";
+import SignUpScreen from "./components/screens/SignUpScreen/SignUpScreen";
 
 function App() {
   return (
@@ -15,8 +16,11 @@ function App() {
             <Route path='/' exact>
               <HomeScreen></HomeScreen>
             </Route>
-            <Route path='/login' exact>
+            <Route path='/login'>
               <LoginScreen></LoginScreen>
+            </Route>
+            <Route path='/signup'>
+              <SignUpScreen></SignUpScreen>
             </Route>
           </Switch>
           
