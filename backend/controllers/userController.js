7a88ee6ce349375c8db7b00856e5b6b355f4e28e
Arefi6 <arefinhossain3@gmail.com>
2021-03-03 13:18:08 +0600

    user Profile backend done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,4 +67,53 @@ const authUser = asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser,authUser}
+//@route:GET  /api/users/profile
+//@desc  user Profile
+//@access private
+
+const getUserProfile = asyncHandler(async(req,res)=>{
+    const user = await User.findById(req.user._id)
+    if(user){
+        res.send({
+           _id:user.id,
+           name:user.name,
+           email:user.email,
+           isAdmin:user.isAdmin,
+        })
+    }
+    else{
+       res.status(404)
+       throw new Error('Invalid user data') 
+    }
+})
+//@route:Put  /api/users/profile
+//@desc  Update user Profile
+//@access private
+
+const updateUserProfile = asyncHandler(async(req,res)=>{
+    const user = await User.findById(req.user._id)
+    if(user){
+
+        user.name= req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if(req.body.password){
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.send({
+           _id:updatedUser.id,
+           name:updatedUser.name,
+           email:updatedUser.email,
+        })
+    }
+    else{
+       res.status(404)
+       throw new Error('Invalid user data') 
+    }
+})
+
+
+export {registerUser,authUser,updateUserProfile,getUserProfile}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { registerUser,authUser } from '../controllers/userController.js';
-
+import { registerUser,authUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import {protect} from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').post(registerUser)
+router.route('/profile')
+.get(protect,getUserProfile)
+.put(protect,updateUserProfile)
 router.post('/login',authUser)
 export default router;
 
