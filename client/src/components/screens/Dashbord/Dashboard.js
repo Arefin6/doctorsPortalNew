0e0c814b48c170f8131c5380304827fee22469b6
Arefi6 <arefinhossain3@gmail.com>
2021-03-06 19:42:32 +0600

    Every thing is  done and dusted

--- client/src/components/screens/Dashbord/Dashboard.js
+++ client/src/components/screens/Dashbord/Dashboard.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { appointmentList } from '../../../Actions/appointmentAction';
+import { getMessageList } from '../../../Actions/messageAction';
+import { getUserList } from '../../../Actions/userAction';
+import Loader from '../../Loader';
+import Message from '../../Message';
 import Sidebar from '../../Sidebar/Sidebar';
 
 const Dashboard = () => {
+
+   const dispatch = useDispatch()
+
+   const userList  = useSelector(state => state.userList)
+   const {error,loading,users} = userList
+
+   const messageList  = useSelector(state => state.messageList)
+  const {messages} = messageList
+
+  const appointmentAll  = useSelector(state => state.appointmentAll)
+  const {appointments} = appointmentAll
+
+
+  useEffect
+   (()=>{
+     dispatch(getMessageList())
+     dispatch(getUserList())
+     dispatch(appointmentList())
+   },[dispatch,getMessageList,getUserList,appointmentList])
     return (
+       <>
+      {error && <Message>{error}</Message>}
+      {loading ? <Loader></Loader>:(
         <div className="row">
             <div className="col-md-3">
             <Sidebar></Sidebar>
@@ -18,7 +46,7 @@ const Dashboard = () => {
        
                    <div className="card-body">
                       
-                      <h1 className="text-center"></h1>
+                      <h1 className="text-center">{appointments.length}</h1>
                       
                    </div>
            </div>
@@ -34,7 +62,7 @@ const Dashboard = () => {
        
                    <div className="card-body">
                       
-                      <h1 className="text-center"></h1>
+                      <h1 className="text-center">{messages.length}</h1>
                       
                    </div>
            </div>
@@ -50,7 +78,7 @@ const Dashboard = () => {
        
                    <div className="card-body">
                       
-                      <h1 className="text-center text-white"></h1>
+                      <h1 className="text-center text-white">{users.length}</h1>
                     
                    </div>
            </div>
@@ -58,7 +86,10 @@ const Dashboard = () => {
            </div>
           
     </div>
+     )}
+     </>
     );
+   
 };
 
 export default Dashboard;
